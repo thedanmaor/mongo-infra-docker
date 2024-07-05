7e487108c043f6b9f1b161b447e69c9dd20be3fc
karl-denby <karl.denby@gmail.com>
2024-07-05 15:58:25 +0100

    clean up option for containers

--- cloud-manager/extras.sh
+++ cloud-manager/extras.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Please choose some extras: 
-extra_options=("pause" "un-pause" "more-servers-x86_64" "more-servers-aarch64" "Quit")
+extra_options=("pause" "un-pause" "more-servers-x86_64" "more-servers-aarch64" "clean-up" "Quit")
 select opt in "${extra_options[@]}"
 do
   case $opt in
@@ -24,6 +24,12 @@ do
       docker compose -f docker-compose-aarch64.yml up -d n4cm n5cm n6cm
       break
       ;;
+    clean-up)
+      echo "Starting n4cm, n5cm & n6cm"
+      docker compose -f docker-compose-aarch64.yml down n1cm n2cm n3cm n4cm n5cm n6cm
+      docker compose -f docker-compose-x86_64.yml down n1cm n2cm n3cm n4cm n5cm n6cm
+      break
+      ;;      
     Quit)
       echo "Bye."
       break
